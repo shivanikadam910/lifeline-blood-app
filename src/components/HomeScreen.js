c6c5cc95f36d08541d88215ffb5a07fdec8fe0ad
Nisarg Thoriya <nisarghthoroya@gmail.com>
2021-04-08 19:34:48 +0530

    home page issue

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,62 +2,68 @@ import React from "react";
 import "../static/home.css";
 import lifeline from "../images/lifeline.png";
 import smile from "../images/smiling-woman.png";
+import donate from "../images/donateVector.png";
 import { Link } from "react-router-dom";
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <div className="container" id="home">
-        <div className="sidebar">
-          <div className="logo">
-            <img src={lifeline} alt="LifeLine" />
-            <h1>LifeLine</h1>
-          </div>
+      <div className="containermain" >
+      <div className="sidebar">
+        
+        <div className="menu">
+          <ul>
+            <li>
+              <div className="menulist">
+                <img src="https://img.icons8.com/fluent-systems-regular/48/000000/dashboard-layout.png" />
+                <h3>Dashboard</h3>
+              </div>
 
-          <div className="menu">
-            <ul>
-              <li>
-                <div className="dashboard">
-                  <img src="https://img.icons8.com/fluent-systems-regular/48/000000/dashboard-layout.png" />
-                  <h3>Dashboard</h3>
-                </div>
+              <div className="menulist">
+                <img src="https://img.icons8.com/material-outlined/24/000000/invite.png" />
+                <h3>Request Blood</h3>
+              </div>
 
-                <div className="requestBlood">
-                  <img src="https://img.icons8.com/material-outlined/24/000000/invite.png" />
-                  <h3>Request Blood</h3>
-                </div>
+              <div className="menulist">
+                <img src="https://img.icons8.com/fluent-systems-regular/48/000000/drop-of-blood.png" />
+                <h3>Donate Blood</h3>
+              </div>
 
-                <div className="donateBlood">
-                  <img src="https://img.icons8.com/fluent-systems-regular/48/000000/drop-of-blood.png" />
-                  <h3>Donate Blood</h3>
-                </div>
+              <div className="menulist">
+                <img src="https://img.icons8.com/material/24/000000/hospital-2.png" />
+                <h3>Hospitals</h3>
+              </div>
 
-                <div className="hospitals">
-                  <img src="https://img.icons8.com/material/24/000000/hospital-2.png" />
-                  <h3>Hospitals</h3>
-                </div>
-
-                <div className="emergency">
-                  <img src="https://img.icons8.com/material-outlined/24/000000/error--v1.png" />
-                  <h3>Emergency</h3>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <div className="why">
-            <h3>Why Donate Blood?</h3>
-            <div className="donateVector"></div>
+              <div className="emergency">
+                <img src="https://img.icons8.com/material-outlined/24/000000/error--v1.png" />
+                <h3>Emergency</h3>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="why">
+          <h3>Why Donate Blood?</h3>
+          
+          <div className="donateVector">
+          <img src={donate} alt="why donate"/>
           </div>
         </div>
+      </div>
+      <div className="container2">
         <div className="banner">
-          <h1>
-            Donate Blood
-            <br />
-            Save lives!
-          </h1>
-          <h3>Donate Now &nbsp; &gt;</h3>
-          <img src={smile} alt="Smiling Woman" />
+            <div class="banner1">
+              <h1>
+                  Donate Blood
+                  <br />
+                  Save lives!
+                </h1>
+                <h3>Donate Now &nbsp; &gt;</h3>
+            </div>
+            <div class="banner2">
+              <img src={smile} alt="Smiling Woman" />
+            </div>
         </div>
+        
         <div className="card-grid">
           <div className="card1">
             <div>My Donations</div>
@@ -75,19 +81,23 @@ class HomeScreen extends React.Component {
             <div className="num">0</div>
           </div>
         </div>
-        <div className="nearby">Nearby Hospitals</div>
-        <div className="hospital-grid">
-          <div className="hospital1">
-            <div className="hospitalName">City Hospital</div>
+        
+          <div className="nearby">Nearby Hospitals</div>
+          <div className="card-grid">
+            <div className="hospital1">
+              <div className="hospitalName">City Hospital</div>
+            </div>
+            <div className="hospital2">
+              <div className="hospitalName">City Hospital</div>
+            </div>
+            <div className="hospital3">
+              <div className="hospitalName">City Hospital</div>
+            </div>
           </div>
-          <div className="hospital2">
-            <div className="hospitalName">City Hospital</div>
-          </div>
-          <div className="hospital3">
-            <div className="hospitalName">City Hospital</div>
-          </div>
-        </div>
+        
       </div>
+       
+    </div>
     );
   }
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,30 +1,36 @@
 import React from "react";
 import firebase from "../firebase/firebase";
+import logo from "../images/logo.png";
 import "../static/navbar.css";
 
 class navbar extends React.Component {
   render() {
     return (
-      <nav class="nav">
-        <div class="container">
-          <ul class="functions">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#" className="signin">
-                Sign-In
-              </a>
-            </li>
-          </ul>
+      <div class="maincontainer">
+        <div class="nav">
+          <div class="navlogo">
+            <img src={logo} alt="LifeLine" /> 
+          </div>
+          <div class="navul">
+            <ul class="ulnav">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#" className="signin">
+                  Sign-In
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </nav>
+      </div>
     );
   }
 }
